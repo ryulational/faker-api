c4bd9320d563e2a61951a8bc974e8af1d62bd8a1
ryulational <97367422+ryulational@users.noreply.github.com>
2023-02-11 13:24:16 -0800

    Install ring and reitit

--- src/faker_api/server.clj
+++ src/faker_api/server.clj
@@ -1,11 +1,16 @@
-(ns faker-api.server (:gen-class))
+(ns faker-api.server
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty])
+  (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/" {:get {:handler (fn [req] {:status 200, :body "Hello World"})}}]])))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+(defn start
+  []
+  (jetty/run-jetty app {:port 3000, :join? false})
+  (println "\nServer running on port 3000"))
+
+(defn -main "I don't do a whole lot ... yet." [& args] (println "main"))
