93a0bd63ccc9e55e609030f9ef2640a6f4ac272f
ryulational <97367422+ryulational@users.noreply.github.com>
2023-02-11 18:01:07 -0800

    Configure Swagger

--- src/faker_api/router.clj
+++ src/faker_api/router.clj
@@ -1,7 +1,26 @@
 (ns faker-api.router
-  (:require [reitit.ring :as ring]
+  (:require [muuntaja.core :as m]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.swagger :as swagger]
+            [reitit.swagger-ui :as swagger-ui]
             [faker-api.faker.routes :as faker]))
+
+(def swagger-docs
+  ["/swagger.json"
+   {:get {:no-doc true,
+          :swagger {:basePath "/",
+                    :info {:title "Faker Server API Reference",
+                           :description "Faker Server API Reference",
+                           :version "0.y.z"}},
+          :handler (swagger/create-swagger-handler)}}])
+
+(def router-config
+  {:data {:muuntaja m/instance,
+          :middleware [swagger/swagger-feature muuntaja/format-middleware]}})
 
 (defn routes
   [env]
-  (ring/ring-handler (ring/router [["/v1" (faker/routes env)]])))
+  (ring/ring-handler
+    (ring/router [swagger-docs ["/v1" (faker/routes env)]] router-config)
+    (ring/routes (swagger-ui/create-swagger-ui-handler {:path "/"}))))
